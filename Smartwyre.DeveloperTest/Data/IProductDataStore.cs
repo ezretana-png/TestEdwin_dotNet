38300b8f60a487b9ff747f581ec6b8d79166f681
Edwin Zuniga <ezretana@gmail.com>
2026-04-27 19:14:43 -0600

    New Interfaces to apply Dependency Inversion

---
+++ Smartwyre.DeveloperTest/Data/IProductDataStore.cs
@@ -0,0 +1,12 @@
+﻿using Smartwyre.DeveloperTest.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smartwyre.DeveloperTest.Data
+{
+    public interface IProductDataStore
+    {
+        Product GetProduct(string productIdentifier);
+    }
+}
---
+++ Smartwyre.DeveloperTest/Data/IRebateDataStore.cs
@@ -0,0 +1,13 @@
+﻿using Smartwyre.DeveloperTest.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smartwyre.DeveloperTest.Data
+{
+    public interface IRebateDataStore
+    {
+        Rebate GetRebate(string rebateIdentifier);
+        void StoreCalculationResult(Rebate account, decimal rebateAmount);
+    }
+}
